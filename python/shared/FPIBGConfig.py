2572f5bf41b7c9171b245b031a4789c69fd2b116
Benjamin Rheault <44026930+brheault@users.noreply.github.com>
2025-02-04 11:45:50 -0500

    Finished getStringField

--- python/shared/FPIBGConfig.py
+++ python/shared/FPIBGConfig.py
@@ -10,7 +10,13 @@ class FPIBGConfig:
             self.configPath = os.path.join(get_repo_root(), "Particle.cfg")
       
       def GetStringField(self, field):
-            print(find_line_with_substring(self.configPath, field))
+            lineString = find_line_with_substring(self.configPath, field)
+            if lineString is None:
+                  print("Error: element not found")
+                  return None
+            else:
+                  fieldString = get_substring(lineString, '=', '\n')
+                  print(fieldString)
 
 
 def get_repo_root():
@@ -23,15 +29,25 @@ def get_repo_root():
     return current_dir
 
 def find_line_with_substring(file_path, substring):
+    """Helper Function to locate the field being requested by finding the line in the config file that begins with that field"""
     with open(file_path, 'r') as file:
         for line in file:
-            if line.startswith(substring):
+            if line.strip().startswith(substring):
                 return line.strip()
     return None
 
+def get_substring(text, start_char, end_char):
+    start_index = text.find(start_char) + 1
+    end_index = len(text) - 1
+
+    if start_index != -1 and end_index != -1:
+        return text[start_index:end_index].strip()
+    else:
+        return None
+
 def main():
       myConfig = FPIBGConfig()
-      myConfig.GetStringField("name")
+      myConfig.GetStringField("cap_name")
 
 if __name__=="__main__":
     main()
