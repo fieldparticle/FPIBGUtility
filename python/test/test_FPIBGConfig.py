55a313e1513c4fe60144e40cb72e4dad5349f102
Benjamin Rheault <44026930+brheault@users.noreply.github.com>
2025-02-10 17:10:43 -0500

    Test Completed

--- python/test/test_FPIBGConfig.py
+++ python/test/test_FPIBGConfig.py
@@ -1,6 +1,31 @@
-from shared import FPIBGConfig
+import os
+import sys
 
-def test_name():
+def get_repo_root():
+    """Gets the absolute path of the git repo root directory."""
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    while not os.path.exists(os.path.join(current_dir, ".git")):
+        current_dir = os.path.dirname(current_dir)
+        if current_dir == "/":
+            raise FileNotFoundError("Could not find .git directory")
+    return current_dir
+
+sharedDir = get_repo_root() + "/python/shared"
+
+sys.path.append(os.path.abspath(sharedDir))
+
+from FPIBGConfig import *
+
+# def test_name():
+#     cObj = FPIBGConfig()
+#     cDict = cObj.GetConfig()
+#     assert cDict.name == "particleOnly"
+
+def main():
     cObj = FPIBGConfig()
-    cDict = cObj.GetConfig()
-    assert cDict.name == "particleOnly"
+    cDic = cObj.GetConfig()
+    print(cDic)
+
+
+if __name__ == "__main__":
+    main()
