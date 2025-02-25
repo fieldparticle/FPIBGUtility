081dc1584c9ae1235e77354877f3e713384ec14d
kristenmoy <147666056+kristenmoy@users.noreply.github.com>
2025-02-25 13:31:28 -0500

    TestFPIBGData
    added paths

---
+++ python/test/TestFPIBGData.py
@@ -0,0 +1,7 @@
+import sys                                                     
+import os                                                     
+syspth = sys.path                                             
+cwd = os.getcwd()                                             
+shrddir = cwd + "\\python\\shared"                            
+sys.path.append(shrddir)                                      
+
