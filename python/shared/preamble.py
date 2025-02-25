0e7b05060c208a3e7b71bdc48c83368dce0f0aa3
JBufl <jbdeletewhendone@gmail.com>
2025-02-25 12:56:27 -0500

    Added a preamble file to copy preambe

---
+++ python/shared/preamble.py
@@ -0,0 +1,10 @@
+###############################################################
+## Preamble to every script. Will append the shared directory #
+import sys                                                    #  
+import os                                                     #
+syspth = sys.path                                             #
+cwd = os.getcwd()                                             #
+shrddir = cwd + "\\python\\shared"                            #
+sys.path.append(shrddir)                                      #
+# Now do imports                                              #
+###############################################################
