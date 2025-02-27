902b6dbf34a0e388caca1e9ff4931da2f4badd72
Benjamin Rheault <44026930+brheault@users.noreply.github.com>
2025-02-27 09:53:02 -0500

    Added Folder Selection
    Added a dialog to select a folder and set it to the value of a lineEdit

--- python/shared/TabFormGenConfig.py
+++ python/shared/TabFormGenConfig.py
@@ -1,16 +1,29 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget,  QFormLayout, QGridLayout, QTabWidget, QLineEdit, QDateEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget,  QFormLayout, QGridLayout, QTabWidget, QLineEdit, QDateEdit, QPushButton, QLabel, QHBoxLayout, QFileDialog
 from PyQt6.QtCore import Qt
+
 
 class TabGenConfig(QTabWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
     def Create(self):
-        # personal page
-        personal_page = QWidget(self)
+        ## Set up a layout object
         layout = QFormLayout()
-        personal_page.setLayout(layout)
-        layout.addRow('First Name:', QLineEdit(self))
-        layout.addRow('Last Name:', QLineEdit(self))
-        layout.addRow('DOB:', QDateEdit(self))
+
+        self.folderLineEdit = QLineEdit()
+        self.browseButton = QPushButton("Browse")
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.folderLineEdit)
+        hbox.addWidget(self.browseButton)
+
+        layout.addRow("Folder:", hbox)
+
+        self.browseButton.clicked.connect(self.browseFolder)
+        self.setLayout(layout)
+    
+    def browseFolder(self):
+        folder = QFileDialog.getExistingDirectory(self, "Select Folder")
+        if folder:
+            self.folderLineEdit.setText(folder)
