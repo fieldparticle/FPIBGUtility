00e112cd094f0d34d5d63a5409f1632f25f0ef83
JBufl <jbdeletewhendone@gmail.com>
2025-03-04 12:44:41 -0500

    More docs

--- python/test/TestMyClass.py
+++ python/test/TestMyClass.py
@@ -12,8 +12,9 @@ sys.path.append(shrddir)                                      #
 
 from FPIBGBase import *
 from MyClass import *
-# First create a base class and name it
+# First instanciate a base class and name it
 bc = FPIBGBase("GlobalBaseClass")
+# Then call create with your configuration file and log file names,
 bc.Create("ParticleJB.cfg",'MyLog.log')
 myClass = MyClass("ExampleObject")
 myClass.Create(bc)
