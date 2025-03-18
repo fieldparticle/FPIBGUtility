dc81200d792139bb5d93c14ebfdad222e63f746b
kristenmoy <147666056+kristenmoy@users.noreply.github.com>
2025-03-18 12:05:38 -0400

    Monkey Patch
    Tests when file does not exist, should reflect in log file

--- python/test/TestFPIBGDataNotExist.py
+++ python/test/TestFPIBGDataNotExist.py
@@ -2,6 +2,7 @@
 ## Preamble to every script. Will append the shared directory #
 import sys                                                    #  
 import os                                                     #
+from unittest import mock                                     #
 syspth = sys.path                                             #
 cwd = os.getcwd()                                             #
 shrddir = cwd + "\\python\\shared"                            #
@@ -25,12 +26,15 @@ match guser:
 myClass = DataClass(True, "ExampleObject")
 myClass.Create(bc,"PQB")
 
-myClass.Open()
-if (myClass.check_data_files() != True):
-    print("Did not work") 
-myClass.create_summary()
-myClass.get_averages()
-print(myClass.average_list)
-    
-bc.Close()
-myClass.Close() 
+# Uses monkey patching to simulate path does not exist
+with mock.patch("os.path.exists", return_value=False):
+    myClass.Open()
+
+    if (myClass.check_data_files() != True):
+        print("Did not work") 
+        myClass.create_summary()
+        myClass.get_averages()
+        print(myClass.average_list)
+
+        bc.Close()
+        myClass.Close()     
