bce0f7d0319b8154483abaffaddf4fe44b6b4f8d
JBufl <jbdeletewhendone@gmail.com>
2025-03-21 11:49:55 -0400

    updated plot class to share

--- python/test/TestFPIBGPlotData.py
+++ python/test/TestFPIBGPlotData.py
@@ -5,7 +5,10 @@ import os                                                     #
 syspth = sys.path                                             #
 cwd = os.getcwd()                                             #
 shrddir = cwd + "\\python\\shared"                            #
-sys.path.append(shrddir)                                      #
+sys.path.append(shrddir)      
+import getpass                                                #
+print(getpass.getuser())                                      #
+guser = getpass.getuser()                                               #
 # Now do imports                                              #
 ###############################################################
 
@@ -13,7 +16,12 @@ from FPIBGBase import *
 from FPIBGPlotData import *
 
 bc = FPIBGBase("GlobalBaseClass")
-bc.Create("ParticleKM.cfg",'KMLog.log')
+#bc.Create("ParticleKM.cfg",'KMLog.log')
+match guser:
+    case "jbwk":
+        bc.Create("ParticleJB.cfg",'MyLog.log')
+    case _:
+        bc.Create("ParticleKM.cfg",'KMLog.log')
 myClass = PlotData("ExampleObject")
 myClass.Create(bc,"PQB")
 myClass.Open()
