77f77ff19d21f65e47e043c2750d47d205812e22
kristenmoy <147666056+kristenmoy@users.noreply.github.com>
2025-03-29 10:44:22 -0400

    Add plots
    Added expectedc column to summary and plotted Frames Per Second vs Loaded Particles and Seconds Per Frame vs Number of Collisions plots

--- python/shared/FPIBGPlotData.py
+++ python/shared/FPIBGPlotData.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
 
 class PlotData:
     def Create(self, BaseObj, file_end):
@@ -40,6 +43,7 @@ class PlotData:
         parts[-1] = parts[-1].replace("data", "")
         return "/".join(parts)
 
+    # Plot cpums vs loadedp
     def plot_cpums(self):
         df = pd.read_csv(self.topdir)
 
@@ -52,6 +56,7 @@ class PlotData:
         plt.grid(True)
         plt.show()
 
+    # Plot B1
     def plot_B1(self):
         df = pd.read_csv(self.topdir)
 
@@ -66,6 +71,7 @@ class PlotData:
         plt.grid(True)
         plt.show()
 
+    # Plot Linearity
     def plot_linearity(self):
         df = pd.read_csv(self.topdir)
 
@@ -73,7 +79,8 @@ class PlotData:
         plt.plot(df['loadedp'], df['cms'] / df['loadedp'], marker='o', linestyle="-", label="(cms/loadedp) vs loadedp")
         plt.plot(df['loadedp'], df['gms'] / df['loadedp'], marker='o', linestyle="-", label="(gms/loadedp) vs loadedp")
         plt.plot(df['loadedp'], (df['cms'] + df['gms']) / df['loadedp'], marker='o', 
-                 linestyle="-", label="(cms+gms)/loadedp vs loadedp")      
+                 linestyle="-", label="(cms+gms)/loadedp vs loadedp")     
+        plt.xlim(82944)
         plt.xlabel("Number of Particles")
         plt.ylabel("Linearity")
         plt.title(f"{os.path.basename(self.topdir)}: Linearity")
@@ -81,6 +88,7 @@ class PlotData:
         plt.grid(True)
         plt.show()
 
+    # Plot Cell Fraction Benchmark
     def plot_cell_fraction(self):
         df = pd.read_csv(self.topdir)
 
@@ -91,6 +99,31 @@ class PlotData:
         plt.xlabel("Side Length")
         plt.ylabel("Seconds per frame, spf (ms)")
         plt.title(f"{os.path.basename(self.topdir)}: Cell Fraction Benchmark")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def plot_fps_vs_loadedp(self):
+        df = pd.read_csv(self.topdir)
+
+        plt.figure(figsize=(8,5))
+        plt.plot(df['loadedp'], df['fps'], marker='o', linestyle='-', label="fps vs loadedp")
+        plt.xlabel("Number of Particles (q)")
+        plt.ylabel("Frames Per Second, fps")
+        plt.title(f"{os.path.basename(self.topdir)}: Frames Per Second vs Loaded Particles")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+    def plot_expectedc(self):
+        df = pd.read_csv(self.topdir)
+        
+        plt.figure(figsize=(8,5))
+        plt.plot(df['expectedc'], df['cms'], marker='o', linestyle='-', label="cms vs loadedp")
+        plt.plot(df['expectedc'], df['gms'], marker='o', linestyle='-', label="gms vs loadedp")
+        plt.xlabel("Number of Collisions (n)")
+        plt.ylabel("Seconds Per Frame, spf(s)")
+        plt.title(f"{os.path.basename(self.topdir)}: Seconds Per Frame vs Number of Collisions")
         plt.legend()
         plt.grid(True)
         plt.show()
