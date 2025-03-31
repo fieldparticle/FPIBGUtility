e526b9b945c6134a378fee5a54f54ecdcdc8e04e
JBufl <jbdeletewhendone@gmail.com>
2025-03-31 14:38:50 -0400

    sprint3 docs 001

--- docs/search/classes_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['parser_0',['Parser',['../classlibconf_1_1_parser.html',1,'libconf']]],
-  ['pptimg2md_1',['pptimg2md',['../classpptimg2md_1_1pptimg2md.html',1,'pptimg2md']]]
-];
