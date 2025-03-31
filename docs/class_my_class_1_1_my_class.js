16204dd0dcd45bf389eb409cd4bb9b4b12bce3ad
JBufl <jbdeletewhendone@gmail.com>
2025-03-31 15:51:50 -0400

    Tryimg to fix pages

---
+++ docs/class_my_class_1_1_my_class.js
@@ -0,0 +1,13 @@
+var class_my_class_1_1_my_class =
+[
+    [ "__init__", "class_my_class_1_1_my_class.html#aa105529815f1e4ebc69d1b36cf960441", null ],
+    [ "Close", "class_my_class_1_1_my_class.html#ac5e15ee3056e68202805b7671195f687", null ],
+    [ "Create", "class_my_class_1_1_my_class.html#ab8b88f2e96e5ea19bfd1ccd308bcfb85", null ],
+    [ "Open", "class_my_class_1_1_my_class.html#ab8fc5ca01a46c5a276392f9a458ce4d2", null ],
+    [ "Read", "class_my_class_1_1_my_class.html#aade95f40b745d1a44c0efd4f62ea4ffb", null ],
+    [ "testObject", "class_my_class_1_1_my_class.html#a78900de93b8b1b8c92f39358011ca9ff", null ],
+    [ "Write", "class_my_class_1_1_my_class.html#aceaa32281b06b3e0a466279f36602429", null ],
+    [ "bobj", "class_my_class_1_1_my_class.html#a751ee0e26e6c80a676eadca4dcd164a7", null ],
+    [ "cfg", "class_my_class_1_1_my_class.html#a7f64328062cd438438ab6675c5fc3473", null ],
+    [ "ObjName", "class_my_class_1_1_my_class.html#ae7f07e8023835ff93013ace49670153a", null ]
+];
